5a04bca3abc6e50b296834ff312696ef8c6e50c5
gtlduytung@gmail.com <gtlduytung@gmail.com>
2021-02-26 17:38:55 +0700

    Add Redux To Product Screen

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstant";
+import { PRODUCT_DETAIL_FAIL, PRODUCT_DETAIL_REQUEST, PRODUCT_DETAIL_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS } from "../constants/productConstant";
 
 export const listProducts = () => async (dispatch) => {
     dispatch({
@@ -10,5 +10,22 @@ export const listProducts = () => async (dispatch) => {
         dispatch({type: PRODUCT_LIST_SUCCESS, payload: data});
     }catch(error){
         dispatch({type: PRODUCT_LIST_FAIL, payload: error.message})
+    }
+}
+
+export const detailsProduct = (productId) => async(dispatch) => {
+    dispatch({
+        type: PRODUCT_DETAIL_REQUEST, payload: productId
+    });
+    try{
+        const {data} = await axios.get(`/api/products/${productId}`);
+        dispatch({type: PRODUCT_DETAIL_SUCCESS, payload: data});
+    }catch(error){
+        dispatch({type: PRODUCT_DETAIL_FAIL, 
+            payload: 
+            error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+        });
     }
 }
