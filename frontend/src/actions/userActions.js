66742e8eeb64cd9fc512981116f1f0703e3916a0
gtlduytung@gmail.com <gtlduytung@gmail.com>
2021-03-03 14:56:37 +0700

    Implement Signin Action

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS } from "../constants/userConstants"
+import { USER_SIGNIN_FAIL, USER_SIGNIN_REQUEST, USER_SIGNIN_SUCCESS, USER_SIGNOUT } from "../constants/userConstants"
 
 export const signin = (email, password) => async (dispatch) => {
     dispatch({type: USER_SIGNIN_REQUEST, payload: {email, password}})
@@ -15,4 +15,10 @@ export const signin = (email, password) => async (dispatch) => {
             : error.message,
         });
     }
+}
+
+export const signout = () => (dispatch) => {
+    localStorage.removeItem('userInfo');
+    localStorage.removeItem('cartItems');
+    dispatch({type: USER_SIGNOUT})
 }
